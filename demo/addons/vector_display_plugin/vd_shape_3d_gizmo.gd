3cc074a40eb1c7632fd55879104d7f9704bad418
Allen Pestaluky <allenpestaluky@gmail.com>
2024-09-02 14:43:39 -0400

    More cleanup for gizmo hack

--- demo/addons/vector_display_plugin/vd_shape_3d_gizmo.gd
+++ demo/addons/vector_display_plugin/vd_shape_3d_gizmo.gd
@@ -3,6 +3,7 @@ class_name VDShape3DGizmo extends EditorNode3DGizmoPlugin
 var mat: StandardMaterial3D
 
 func _init() -> void:
+	cleanup()
 	create_material("main", Color(0.5,1,0.5))
 	mat = get_material("main") # saving mat to work around Godot issue #96481
 
@@ -10,20 +11,18 @@ func _get_gizmo_name() -> String:
 	return "VDShape3D Gizmo"
 
 func _has_gizmo(node: Node3D) -> bool:
-	GizmoUpdater.gizmo = null
-	GizmoUpdater.gizmo_plugin = null
+	cleanup()
 	return node is VDShape3D
 
 func _redraw(gizmo: EditorNode3DGizmo) -> void:
 	gizmo.clear()
-	# disable this and reload project to prevent crash:
 	GizmoUpdater.gizmo = gizmo
 	GizmoUpdater.gizmo_plugin = self
 
 	var node3d: Node3D = gizmo.get_node_3d()
 	var shape := node3d as VDShape3D
 	var world_space_samples: Array[PackedVector4Array] = shape.get_samples_3d(1.0);
-	add_samples_as_lines(world_space_samples, gizmo, mat);
+	add_samples_as_lines(world_space_samples, gizmo, mat); # should be get_material("main", gizmo), but this works around Godot issue #96481
 
 static func add_samples_as_lines(samples: Array[PackedVector4Array], gizmo: EditorNode3DGizmo, material: StandardMaterial3D) -> void:
 	var lines := PackedVector3Array()
@@ -38,3 +37,11 @@ static func add_samples_as_lines(samples: Array[PackedVector4Array], gizmo: Edit
 					next_sample += Vector4(0.01, 0.01, 0.01, 0.0) # TODO: make this better
 				lines.push_back(Vector3(next_sample.x, next_sample.y, next_sample.z))
 	gizmo.add_lines(lines, material, false)
+
+func _notification(what: int) -> void:
+	if what == NOTIFICATION_PREDELETE:
+		cleanup()
+
+func cleanup() -> void:
+	GizmoUpdater.gizmo = null
+	GizmoUpdater.gizmo_plugin = null
