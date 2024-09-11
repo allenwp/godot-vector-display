e35d864ce90d3abead265f63379480faa449a5cd
Allen Pestaluky <allenpestaluky@gmail.com>
2024-09-11 14:40:40 -0400

    Added an animated shapes scene

--- demo/roate.gd
+++ demo/roate.gd
@@ -1,11 +1,24 @@
-extends Node3D
+extends Node
+
+## If null, get_parent() will be used instead.
+@export var target: Node3D = null
 
 @export var speed: float = 0.01;
 @export var around_z: bool = false #if true, it will rotate around y
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _physics_process(_delta: float) -> void:
-	if around_z:
-		rotate_z(speed);
+	var node_3d: Node3D
+	if target != null:
+		node_3d = target
 	else:
-		rotate_y(speed);
+		var parent: Node = get_parent()
+		if parent is Node3D:
+			node_3d = parent as Node3D
+
+	if node_3d == null:
+		printerr("Rotate script requires a target or parent Node3D.")
+	else:
+		if around_z:
+			node_3d.rotate_z(speed)
+		else:
+			node_3d.rotate_y(speed);
